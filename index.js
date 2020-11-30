8e85b4b48e36f68652f4b5bfecbdfd104d55a07d
Ari <etin22@ncssm.edu>
2020-11-30 13:46:17 -0500

    edited js. Does not work. Examine eval()

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function main(){
-  console.log('hi');
+class nEvent {
+  constructor(eName,time=0,date=0) {
+    this.eName=eName;
+    this.time=time;
+    this.date=date;
+  }
+  info()
+  {
+    alert(`${this.eName} is at ${this.time} on ${this.date}`)
+  }
+}
+function newEvent(){
+  let eName=document.getElementById("eName").value;
+  let `${eName}`= new nEvent(eName);
+  eName.info();
 }
