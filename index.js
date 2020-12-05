2db6c8f84c24a037fba826a207ad7deaa0850833
Ari <etin22@ncssm.edu>
2020-12-05 17:40:22 -0500

    fixed scaling added remove feature started work on displaying time

--- index.js
+++ index.js
@@ -15,13 +15,20 @@ class nEvent {
     var canvas = document.getElementById("calCan");
     var ctx = canvas.getContext("2d");
     ctx.fillStyle="#FF0000";
-    ctx.fillRect(this.date, this.time, document.getElementById('calendar').offsetWidth/7, this.length);
+    ctx.fillRect(this.date*document.getElementById('calendar').offsetWidth/7, this.time, document.getElementById('calendar').offsetWidth/7, this.length);
     ctx.fillStyle="#FFFFFF";
     ctx.font = "12px Arial";
-    ctx.fillText(this.eName, this.date+10, this.time+20);
+    ctx.fillText(this.eName, this.date*document.getElementById('calendar').offsetWidth/7+10, this.time+20);
+    ctx.fillText(`${toTime(this.time)}-${toTime(this.time+this.length)}`, this.date*document.getElementById('calendar').offsetWidth/7+10, this.time+40);
   }
 }
+
+function toTime (time){
+  return (time<10? `000${time}`: time<100? `00${time}`: time<1000? `0${time}`: `${time}`);
+}
+
 var eList = {};
+
 function newEvent(){
   //get input from document. time and date not yet in html
   let name=document.getElementById("eName").value;
@@ -30,7 +37,7 @@ function newEvent(){
   let length=Number(document.getElementById("length").value);
 
   //dynamically add new instance in object, allowing for bracket notation call
-  eList[name]= new nEvent(name,time,date);
+  eList[name]= new nEvent(name,time,date,length);
   //testing
   eList[name].info();
   eList[name].display();
@@ -45,7 +52,22 @@ function newEvent(){
     window.addEventListener('resize',init,false);
   }
   function init() {
-    document.getElementById('calCan').getContext('2d').canvas.width =(document.getElementById('calendar').offsetWidth)-15;
+    document.getElementById('calCan').getContext('2d').canvas.width =(document.getElementById('calendar').offsetWidth);
     document.getElementById('calCan').getContext('2d').canvas.height = 2400;
+    document.getElementById('calendar').style.height = `${window.innerHeight-(document.getElementsByTagName('h1')[0].offsetHeight+51)}px`;
+    document.getElementById('form').style.height = `${window.innerHeight-(document.getElementsByTagName('h1')[0].offsetHeight+51)}px`;
+    for (e in eList){
+      eList[e].display();
+    }
+  }
+}
+function remove (){
+  let eRemove=document.getElementById('eRemove').value;
+  let canvas = document.getElementById('calCan');
+  let ctx = canvas.getContext('2d');
+  delete eList[`${eRemove}`];
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (e in eList){
+    eList[e].display();
   }
 }
